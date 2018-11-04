ed28c012dac64392bdf1ccd924ed6f0d8069118f
GokiSakuraba <goki14914@icloud.com>
2018-11-05 02:43:17 +0900

    fix syntax

--- config/application.rb
+++ config/application.rb
@@ -27,10 +27,7 @@ module Diary
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Don't generate system test files.
     config.generators do |g|
-      # 色々な記述があるので、一番下に追記する
       g.test_framework :rspec,
                        fixtures: true,
                        view_specs: false,
