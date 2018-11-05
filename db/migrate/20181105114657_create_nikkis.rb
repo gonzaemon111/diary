ff2f46e41a9ea5ef0987347d58853b9f41d0ce67
GokiSakuraba <goki14914@icloud.com>
2018-11-05 20:54:46 +0900

    add index and null configuration

--- db/migrate/20181105114657_create_nikkis.rb
+++ db/migrate/20181105114657_create_nikkis.rb
@@ -1,8 +1,8 @@
 class CreateNikkis < ActiveRecord::Migration[5.2]
   def change
     create_table :nikkis do |t|
-      t.string :value
-      t.datetime :datetime
+      t.string :value, index: true, null: false
+      t.datetime :datetime, index: true, null: false
 
       t.timestamps
     end
