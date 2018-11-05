ef97a48d9050a64ae678161474d996854076f7ef
GokiSakuraba <goki14914@icloud.com>
2018-11-06 00:15:50 +0900

    add newfile for shoulda-matchers

---
+++ spec/support/shoulda_matchers.rb
@@ -0,0 +1,9 @@
+require "rspec/rails"
+require "shoulda-matchers"
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
