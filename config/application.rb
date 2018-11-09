c47cc28ad3cabcc584fe8d343fc6771bf30d5ac2
GokiSakuraba <goki14914@icloud.com>
2018-11-09 20:53:44 +0900

    add variables for TZ

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Dotenv::Railtie.load
 module Diary
   class Application < Rails::Application
     config.load_defaults 5.2
-    config.time_zone = "Asia/Tokyo"
+    config.time_zone = ENV["TZ"]
 
     config.generators do |g|
       g.test_framework :rspec,
