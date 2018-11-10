6e2c104114a6b0ff340b57f798bbb19933955ce0
GokiSakuraba <goki14914@icloud.com>
2018-11-11 03:24:28 +0900

    execute rubocop -a

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 module Constants
   ## Constants::NUMでアクセスできる
-  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.freeze
 end
