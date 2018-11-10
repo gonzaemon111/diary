1a0fb6d700700c12783505e05b5ac9404af68b4c
GokiSakuraba <goki14914@icloud.com>
2018-11-10 22:26:38 +0900

    add spec factory for omniauth_profile

--- spec/factories/omniauth_profiles.rb
+++ spec/factories/omniauth_profiles.rb
@@ -4,11 +4,11 @@ FactoryBot.define do
     uid { "MyString" }
     name { "MyString" }
     provider { 0 }
-    image_url Rack::Test::UploadedFile.new(File.join(Rails.root, "public/images/book.png"))
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, "public/images/book.png")) }
     description { "MyString" }
     email { "MyString" }
     token { "MyString" }
-    secret { "MyString" }
+    refresh_token { "MyString" }
     created_at { Faker::Time.between(5.days.ago, 3.days.ago, :all) }
     updated_at { Faker::Time.between(2.days.ago, 1.days.ago, :all) }
   end
