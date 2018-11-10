e1be4d793f2710a6e76a595f489cf011df483840
GokiSakuraba <goki14914@icloud.com>
2018-11-10 22:30:06 +0900

    add conffiguration for all controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    #strong parametersを設定し、usernameを許可
+    devise_parameter_sanitizer.for(:sign_up) {
+      |u| u.permit(
+        :name,
+        :password,
+        :password_confirmation
+      )
+    }
+  end
 end
