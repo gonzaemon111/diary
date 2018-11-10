c42e13077cf1f38cc8f3747fe92b2bc9711b0751
GokiSakuraba <goki14914@icloud.com>
2018-11-10 14:59:34 +0900

    add carrierwave configuration for rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,4 +21,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
+  # carrierwaveでできたファイルを削除
+  config.after(:all) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"]) if Rails.env.test?
+  end
 end
