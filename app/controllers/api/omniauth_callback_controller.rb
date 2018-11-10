db50155a274c66be4af1119656fb36b6349d5910
GokiSakuraba <goki14914@icloud.com>
2018-11-10 22:32:21 +0900

    not perfect in the middle

--- app/controllers/api/omniauth_callback_controller.rb
+++ app/controllers/api/omniauth_callback_controller.rb
@@ -1,4 +1,26 @@
 module Api
   class OmniauthCallbackController < Devise::OmniauthCallbacksController
+    def line
+      line_omniauth
+    end
+
+    private
+    def line_omniauth
+      current_user_id = current_user ? current_user : nil
+      result = Api::Line::OmniauthUsecase.new(request.env['omniauth.auth'], current_user_id).execute
+      @user = result[:user]
+      if @user.persisted? || result[:omniauth_profile].persisted?
+        # head :ok
+        sign_in_and_redirect @user
+      else
+        logger.debug "こっちか"
+        head :unprocessable_entity
+      end
+    end
+
+    def after_sign_in_path_for
+      #リダイレクトしたいパス
+      root_path
+     end
   end
 end
