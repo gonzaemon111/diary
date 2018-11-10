c0b36fcee55cf3163e13f3c015d99b0be1407446
GokiSakuraba <goki14914@icloud.com>
2018-11-10 22:29:35 +0900

    fix for spec/model for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,9 +17,7 @@ RSpec.describe User, type: :model do
   context "validation" do
     subject { create(:user) }
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
-    it { is_expected.to validate_presence_of(:password_confirmation) }
     it { is_expected.to validate_presence_of(:sign_in_count) }
     it { is_expected.to validate_presence_of(:failed_attempts) }
 
@@ -32,7 +30,6 @@ RSpec.describe User, type: :model do
 
     it { is_expected.to validate_length_of(:name).is_at_least(1) }
     it { is_expected.to validate_length_of(:password).is_at_least(6) }
-    it { is_expected.to validate_length_of(:password_confirmation).is_at_least(6) }
 
     it { is_expected.to allow_value(*valid_emails).for(:email) }
     it { is_expected.not_to allow_value(*invalid_emails).for(:email) }
