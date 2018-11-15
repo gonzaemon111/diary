c1b38c56726f4c51f40fc65ea69e2d86ae2d9b6e
GokiSakuraba <goki14914@icloud.com>
2018-11-16 00:50:40 +0900

    add bubble_container message

---
+++ app/factories/api/line/messages/flexes/bubble_container.rb
@@ -0,0 +1,53 @@
+module Api
+  module Line
+    module Messages
+      module Flexes
+        class BubbleContainer
+          def initialize; end
+          def execute
+            {
+              "type": "bubble",
+              "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Header text"
+                  }
+                ]
+              },
+              "hero": {
+                "type": "image",
+                "url": "https://example.com/flex/images/image.jpg"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Body text"
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Footer text"
+                  }
+                ]
+              },
+              "styles": {
+                "comment": "See the example of a bubble style object"
+              }
+            }
+          end
+        end
+      end
+    end
+  end
+end
