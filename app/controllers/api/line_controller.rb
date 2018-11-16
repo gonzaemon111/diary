5c7da599310d2c2c995996b78807cdebc9f30b63
GokiSakuraba <goki14914@icloud.com>
2018-11-16 19:35:08 +0900

    add strong parameter

--- app/controllers/api/line_controller.rb
+++ app/controllers/api/line_controller.rb
@@ -6,12 +6,44 @@ module Api
     protect_from_forgery except: [:callback]
 
     def callback
-      result = Api::Line::BotUsecase.new(request).execute
+      result = Api::Line::ReceiveUsecase.new(request, receive_params).execute
       if result
         head :ok
       else
         head 400
       end
+    end
+
+    private
+
+    def receive_params
+      params.permit(
+        :destination,
+        events: [
+          :type,
+          :replyToken,
+          {
+            source: %i[
+              userId
+              type
+            ]
+          },
+          :timestamp,
+          {
+            message: [
+              :type,
+              :id,
+              :text,
+              {
+                contentProvider: %i[type]
+              }
+            ]
+          },
+          {
+            postback: %i[data]
+          }
+        ]
+      )
     end
   end
 end
