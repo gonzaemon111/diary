7fb0c83050799592ae83c046ab97b83ebb48dba1
GokiSakuraba <goki14914@icloud.com>
2018-11-16 19:50:23 +0900

    add message image

---
+++ app/factories/api/line/events/messages/image.rb
@@ -0,0 +1,20 @@
+module Api
+  module Line
+    module Events
+      module Messages
+        class Image
+          def initialize(event)
+            @event = event
+          end
+  
+          def execute
+            {
+              type: "text",
+              text: "ごめん、画像はぼっとできないんだ、、、"
+            }
+          end
+        end
+      end
+    end
+  end
+end
