ab3f7d3f122c607cf72fa58c6b2da3f489bbe652
GokiSakuraba <goki14914@icloud.com>
2018-11-16 19:51:03 +0900

    add message text

---
+++ app/factories/api/line/events/messages/text.rb
@@ -0,0 +1,20 @@
+module Api
+  module Line
+    module Events
+      module Messages
+        class Text
+          def initialize(event)
+            @event = event
+          end
+  
+          def execute
+            {
+              type: "text",
+              text: "#{@event.message["text"]}\nって送ってくれてありがとう💙💚💛💜💗🤗"
+            }
+          end
+        end
+      end
+    end
+  end
+end
