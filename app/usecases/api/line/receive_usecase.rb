11573dec14e914979063a4385a84188387dd3159
GokiSakuraba <goki14914@icloud.com>
2018-11-16 19:43:19 +0900

    fix from request to params

---
+++ app/usecases/api/line/receive_usecase.rb
@@ -0,0 +1,63 @@
+module Api
+  module Line
+    class ReceiveUsecase
+      def initialize(request, params)
+        @request = request
+        @client ||= client
+        @params = params
+      end
+
+      def execute
+        Rails.logger.debug "ストロングパラメータ #{@params["destination"]}"
+        body = @request.body.read
+
+        signature = @request.env["HTTP_X_LINE_SIGNATURE"]
+
+        unless @client.validate_signature(body, signature)
+          error 400 do "Bad Request" end
+        end
+
+        events = @client.parse_events_from(body)
+
+        Rails.logger.debug "---------#{events.class}"
+
+        @params["events"].each { |event|
+          profiles = []
+          Rails.logger.debug "#{profiles}"
+          profile = Profile
+                      .where(uid: event["source"]["userId"])
+                      .where(provider: 0)
+                      .first
+
+          Rails.logger.debug "#{profile.nil?}"
+          Rails.logger.debug "#{profile}" if profile.present?
+
+          unless profile.present?
+            new_profile = Profile.create!(uid: event["source"]["userId"],provider: 0)
+            profiles.push new_profile
+          end
+          result = Api::Line::Events::BaseFactory.new(event, @client).execute
+          return false unless result
+        }
+        true
+      end
+
+      private
+
+      def client
+        case Rails.env
+        when "development"
+          ::Line::Bot::Client.new { |config|
+            config.channel_secret = Settings.line.messaging_api.channel_secret
+            config.channel_token = Settings.line.messaging_api.channel_token
+          }
+        else
+          ::Line::Bot::Client.new { |config|
+            config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+            config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+          }
+        end
+      end
+    end
+  end
+end
