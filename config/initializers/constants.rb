719d0e54a212ef1a9be118e882cc301019147e86
GokiSakuraba <goki14914@icloud.com>
2018-11-16 19:37:35 +0900

    add constants for URL between local and deploy

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,10 @@
 module Constants
   ## Constants::NUMでアクセスできる
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.freeze
+  URL = case Rails.env
+        when "production"
+          "https://gonzodiary.herokuapp.com"
+        else
+          Settings.network.url.ngrok
+        end
 end
