64aac430781013c605ae7831ee4d4e87f08fbbc6
GokiSakuraba <goki14914@icloud.com>
2018-11-16 19:51:52 +0900

    add template

---
+++ app/factories/api/line/events/messages/templates/confirm_template.rb
@@ -0,0 +1,36 @@
+module Api
+  module Line
+    module Events
+      module Messages
+        module Templates
+          class ConfirmTemplate
+            def initialize; end
+  
+            def execute
+              {
+                "type": "template",
+                "altText": "this is a confirm template",
+                "template": {
+                  "type": "confirm",
+                  "text": "Are you sure?",
+                  "actions": [
+                    {
+                      "type": "message",
+                      "label": "Yes",
+                      "text": "yes"
+                    },
+                    {
+                      "type": "message",
+                      "label": "No",
+                      "text": "no"
+                    }
+                  ]
+                }
+              }
+            end
+          end
+        end
+      end
+    end
+  end
+end
