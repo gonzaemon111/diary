417fc5d80e6e7d05da6aabd1733924ee7f358a30
GokiSakuraba <goki14914@icloud.com>
2018-11-16 20:00:23 +0900

    remove logger

--- app/factories/api/line/events/messages/base.rb
+++ app/factories/api/line/events/messages/base.rb
@@ -11,7 +11,6 @@ module Api
           end
 
           def execute
-            Rails.logger.debug "Api::Line::Events::Messages::Basedesu"
             message = case @event["message"]["type"]
                       when "text"
                         Api::Line::Events::Messages::Templates::ButtonTemplate.new.execute
@@ -20,8 +19,6 @@ module Api
                       when "sticker"
                         Api::Line::Events::Messages::Sticker.new(@event).execute
                       end
-            Rails.logger.debug "message -- #{message}"
-            Rails.logger.debug "eirgb --- #{@event["replyToken"]}"
             @client.reply_message(@event["replyToken"], message)
           end
         end
