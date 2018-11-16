d0f23c85168f205eb8a563106b1a9fbe31147e1a
GokiSakuraba <goki14914@icloud.com>
2018-11-16 20:15:16 +0900

    fix constants url

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,9 +2,9 @@ module Constants
   ## Constants::NUMでアクセスできる
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.freeze
   URL = case Rails.env
-        when "production"
-          "https://gonzodiary.herokuapp.com"
-        else
+        when "development"
           Settings.network.url.ngrok
+        else
+          "https://gonzodiary.herokuapp.com"
         end
 end
