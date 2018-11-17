04c5abaacb764c4b75d9387b891e7f7dacf2ca84
GokiSakuraba <goki14914@icloud.com>
2018-11-17 20:39:29 +0900

    add association spec for user

--- spec/models/nikki_spec.rb
+++ spec/models/nikki_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Nikki, type: :model do
     it { is_expected.to validate_presence_of(:value) }
     it { is_expected.to validate_presence_of(:datetime) }
   end
+  context "association" do
+    subject { create(:nikki) }
+    it { is_expected.to belong_to(:user) }
+  end
 end
