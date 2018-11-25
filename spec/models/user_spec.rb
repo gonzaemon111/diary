59b4b57799001af02a224cecbd99bf48cb422f7a
GokiSakuraba <goki14914@icloud.com>
2018-11-25 15:38:03 +0900

    execute rubocop for automode

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,9 +44,9 @@ RSpec.describe User, type: :model do
 
   context "db" do
     subject { create(:user) }
-    it { is_expected.to have_db_column(:name).of_type(:string).with_options(default: "",null: false) }
+    it { is_expected.to have_db_column(:name).of_type(:string).with_options(default: "", null: false) }
     it { is_expected.to have_db_column(:email).of_type(:string) }
-    it { is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(default: "",null: false) }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(default: "", null: false) }
     it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
     it { is_expected.to have_db_column(:reset_password_sent_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:remember_created_at).of_type(:datetime) }
@@ -60,7 +60,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column(:unlock_token).of_type(:string) }
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
-
 
     it { should have_db_index(:confirmation_token) }
     it { should have_db_index(:reset_password_token) }
