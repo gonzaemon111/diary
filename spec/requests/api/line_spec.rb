224e25dd0ece9030d5dce971012ab61f58344295
GokiSakuraba <goki14914@icloud.com>
2018-11-26 01:30:02 +0900

    execute rubocop for automode

--- spec/requests/api/line_spec.rb
+++ spec/requests/api/line_spec.rb
@@ -1,39 +1,39 @@
 require "rails_helper"
 
 MESSAGE_TEXT_PARAMS = {
-  "events"=>[
+  "events" => [
     {
-      "type"=>"message",
-      "replyToken"=>"b1d8bd1903f144378f452bc78b4f53ff",
-      "source"=> {
-        "userId"=>"xxxxxxxxx",
-        "type"=>"user"
+      "type" => "message",
+      "replyToken" => "b1d8bd1903f144378f452bc78b4f53ff",
+      "source" => {
+        "userId" => "xxxxxxxxx",
+        "type" => "user"
       },
-      "timestamp"=>1542212801054,
-      "message"=>{
-        "type"=>"text",
-        "id"=>"8863567885824",
-        "text"=>"test"
-       }
-     }
-   ]
-  }.freeze
+      "timestamp" => 1_542_212_801_054,
+      "message" => {
+        "type" => "text",
+        "id" => "8863567885824",
+        "text" => "test"
+      }
+    }
+  ]
+}.freeze
 
 MESSAGE_IMAGE_PARAMS = {
-  "events"=>[
+  "events" => [
     {
-      "type"=>"message",
-      "replyToken"=>"18abc860c89c4b4a9b240e010df18126",
-      "source"=>{
-        "userId"=>"xxxxxxxxx",
-        "type"=>"user"
+      "type" => "message",
+      "replyToken" => "18abc860c89c4b4a9b240e010df18126",
+      "source" => {
+        "userId" => "xxxxxxxxx",
+        "type" => "user"
       },
-      "timestamp"=>1542213286231,
-      "message"=>{
-        "type"=>"image",
-        "id"=>"8863586432435",
-        "contentProvider"=>{
-          "type"=>"line"
+      "timestamp" => 1_542_213_286_231,
+      "message" => {
+        "type" => "image",
+        "id" => "8863586432435",
+        "contentProvider" => {
+          "type" => "line"
         }
       }
     }
@@ -41,16 +41,16 @@ MESSAGE_IMAGE_PARAMS = {
 }.freeze
 
 MESSAGE_STICKER_PARAMS = {
-  "events"=>[
+  "events" => [
     {
-      "type"=>"message",
-      "replyToken"=>"18abc860c89c4b4a9b240e010df18126",
-      "source"=>{
-        "userId"=>"xxxxxxxxx",
-        "type"=>"user"
+      "type" => "message",
+      "replyToken" => "18abc860c89c4b4a9b240e010df18126",
+      "source" => {
+        "userId" => "xxxxxxxxx",
+        "type" => "user"
       },
-      "timestamp"=>1542213286231,
-      "message"=>{
+      "timestamp" => 1_542_213_286_231,
+      "message" => {
         "type": "sticker",
         "packageId": "1",
         "stickerId": "1"
@@ -60,34 +60,34 @@ MESSAGE_STICKER_PARAMS = {
 }.freeze
 
 MESSAGE_TEXT_DIARY_PARAMS = {
-  "events"=>[
+  "events" => [
     {
-      "type"=>"message",
-      "replyToken"=>"b1d8bd1903f144378f452bc78b4f53ff",
-      "source"=> {
-        "userId"=>"xxxxxxxxx",
-        "type"=>"user"
+      "type" => "message",
+      "replyToken" => "b1d8bd1903f144378f452bc78b4f53ff",
+      "source" => {
+        "userId" => "xxxxxxxxx",
+        "type" => "user"
       },
-      "timestamp"=>1542212801054,
-      "message"=>{
-        "type"=>"text",
-        "id"=>"8863567885824",
-        "text"=>"日記頑張った"
-       }
-     }
-   ]
-  }.freeze
+      "timestamp" => 1_542_212_801_054,
+      "message" => {
+        "type" => "text",
+        "id" => "8863567885824",
+        "text" => "日記頑張った"
+      }
+    }
+  ]
+}.freeze
 
 FOLLOW_EVENT_PARAMS = {
-  "events"=>[
+  "events" => [
     {
-      "type"=>"follow",
-      "replyToken"=>"5c24eb5d27ca4cba834681205c92542a",
-      "source"=>{
-        "userId"=>"Ud01d8e3648823376d3f5ba6c6345d392",
-        "type"=>"user"
-       },
-       "timestamp"=>1542288602614
+      "type" => "follow",
+      "replyToken" => "5c24eb5d27ca4cba834681205c92542a",
+      "source" => {
+        "userId" => "Ud01d8e3648823376d3f5ba6c6345d392",
+        "type" => "user"
+      },
+      "timestamp" => 1_542_288_602_614
     }
   ]
 }.freeze
