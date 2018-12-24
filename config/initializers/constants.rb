db07b76173edcfb0ae42d548a299a4b508040319
GokiSakuraba <goki14914@icloud.com>
2018-12-24 16:55:21 +0900

    fix DAYARRAY in Constants.rb

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,15 +1,9 @@
 module Constants
   ## Constants::NUMでアクセスできる
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.freeze
-  DAY_ARRAY = {
-    "0": "日",
-    "1": "月",
-    "2": "火",
-    "3": "水",
-    "4": "木",
-    "5": "金",
-    "6": "土"
-  }.freeze
+  DAY_ARRAY = [
+    "日", "月", "火", "水", "木", "金", "土"
+  ].freeze
   URL = case Rails.env
         when "development"
           Settings.network.url.ngrok
