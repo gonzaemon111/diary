cc48da1e2051d709dfe17af64b9a336bb0b3a870
GokiSakuraba <goki14914@icloud.com>
2018-12-24 17:08:07 +0900

    execute rubocop

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,8 +1,8 @@
 module Constants
   ## Constants::NUMでアクセスできる
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.freeze
-  DAY_ARRAY = [
-    "日", "月", "火", "水", "木", "金", "土"
+  DAY_ARRAY = %w[
+    日 月 火 水 木 金 土
   ].freeze
   URL = case Rails.env
         when "development"
